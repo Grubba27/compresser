a929c5713502bbc209e88f47de1e38c3ba844b07
Grubba27 <70247653+Grubba27@users.noreply.github.com>
2021-08-08 11:00:43 -0300

    adjusted the for loop

--- decompresser.py
+++ decompresser.py
@@ -9,13 +9,8 @@ original_instructions.close()
 with open('comprimido.txt') as original:
     compressed = original.readline()
 
-no_break_lines = compressed.replace("0x", "\n")
-
-descompressed_spaces = no_break_lines.replace("1x", " ")
-
-descompressing_com = descompressed_spaces.replace("2x", "com")
-
-descompressing_as = descompressing_com.replace("3x", "as")
+for key in instructions_dict:
+    compressed = compressed.replace(instructions_dict[key], key)
 
 new_text = open('descomprimido.txt', 'w')
-new_text.write(descompressing_as)
+new_text.write(compressed)
